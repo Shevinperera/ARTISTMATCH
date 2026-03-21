6d70542018d1c4cae041c6360228d236b2d57cf1
Shevin Perera <2005shevinperera@gmail.com>
2026-03-21 17:04:31 +0530

    main logos added to assests folder

---
+++ frontend/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'Pages/login_page.dart'; 
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Now it is safe to boot up the app!
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      
+      
+      home: SignInScreen(), 
+    );
+  }
+}
